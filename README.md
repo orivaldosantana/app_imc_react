6f255177ab6b7c020179b0d75b3a849e89270de3
Orivaldo Santana Jr <orivaldo@gmail.com>
2021-08-01 18:20:03 -0300

    Classe form com com evento do botão calcular

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # App IMC - React!
+
+
+## Referências 
+
+* [Guia com os principais conceitos React](https://pt-br.reactjs.org/docs/hello-world.html)
--- src/components/form.js
+++ src/components/form.js
@@ -1,22 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react'
 
 import './form.css'
 
 
-export default function IMCForm(props) {
+export default class  IMCForm extends Component {
+    constructor(props){
+        super(props);
+        this.state = {mass: '', hight: ''}; 
 
-    return ( 
-        <div className="Form" > 
-            
-            <label> <strong> Massa: </strong></label>
-            <div> 
-                <input id="massa" /> 
-            </div>
-            <label><strong> Altura: </strong> </label>
-            <div> 
-                <input id="altura" />
-            </div>
-            <button type="button"> Calcular </button>
-        </div>
-    );
+        
+        this.handleSubmit = this.handleSubmit.bind(this); 
+        this.handleChangeMass = this.handleChangeMass.bind(this); 
+        this.handleChangeHight = this.handleChangeHight.bind(this);
+    }
+
+    handleChangeMass(event){
+        this.setState({mass: event.target.value }); 
+    }
+
+    handleChangeHight(event){
+        this.setState({hight: event.target.value}); 
+    }
+    
+    handleSubmit(event){
+        console.log("Dados do form: "+ this.state.mass +" "+ this.state.hight )
+        //evita atualizar a página 
+        event.preventDefault(); 
+    }
+
+    render() {
+        return ( 
+            <form className="Form" onSubmit={this.handleSubmit} >
+                <label> <strong> Massa: </strong></label>
+                <div> 
+                    <input type="number" value={this.state.mass} onChange={this.handleChangeMass} id="massa" /> 
+                </div>
+                <label><strong> Altura: </strong> </label>
+                <div> 
+                    <input type="number" value={this.state.hight} onChange={this.handleChangeHight} id="altura" />
+                </div>
+                <div className="BtnSubmit"> 
+                    <input type="submit" value="Calcular"/> 
+                </div>
+            </form>
+        );
+    }
 }
