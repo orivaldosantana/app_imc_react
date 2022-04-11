2579f5fe7dd2bf3710b533f128702e2812549071
Orivaldo Santana Jr <orivaldo@gmail.com>
2022-04-11 20:32:21 -0300

    manipulando evento na caixa de entrada da massa

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,25 @@
 import React, { Component } from "react";
 
 class IMCForm extends Component {
+    constructor(props){
+        super(props) 
+        this.state = {mass: '', hight: ''};
+
+        this.handleChangeMass = this.handleChangeMass.bind(this);  
+    }
+
+    handleChangeMass(event){
+        
+        this.setState( {mass: event.target.value} ); 
+        console.log(event.target.value) 
+    }
 
     render(){
         return(
             <form> 
                 <div>
                     <label>Massa: </label>
-                    <input type="number" id="massa" />
+                    <input type="number" id="massa" onChange={this.handleChangeMass} />
                 </div>
                 <div>
                     <label>Massa: </label>
@@ -16,7 +28,6 @@ class IMCForm extends Component {
                 <div>
                     <input type="submit" value="Calcular" />
                 </div>
-
             </form>
         ) 
     }; 
