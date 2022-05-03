f250046c14f313e415862261a9c2bb63590bd786
Orivaldo Santana Jr <orivaldo@gmail.com>
2022-05-03 12:37:56 -0300

    criando o cabeçalho

---
+++ src/componets/IMCHeader.js
@@ -0,0 +1,13 @@
+import React from 'react' 
+import '../App.css' 
+
+function IMCHeader(props){
+
+    return (
+        <header>
+            <h1> {props.title} </h1>
+        </header>
+    );
+}
+
+export default IMCHeader; 
